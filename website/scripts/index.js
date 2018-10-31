0416b87b21631af8230bc5059f84236af9511121
Carlos Requena <crequena@ulb.ac.be>
2018-10-31 22:47:36 +0100

    move vue-root to an outer component
    fix navicon
    update npm packages

--- website/scripts/index.js
+++ website/scripts/index.js
@@ -2,9 +2,26 @@ import Materialize from 'materialize-css/dist/js/materialize.min';
 import EvilIcons from 'evil-icons/assets/evil-icons.min.js';
 import EvilIconsCss from 'evil-icons/assets/evil-icons.min.css';
 
+import Vue from 'vue';
+
+import Post from './Post.vue';
+import Blog from './Blog.vue';
+
 import css from 'Styles/main.scss';
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
+});
+
+/* Plugins come before new Vue instance */
+Vue.use(require('vue-moment'));
+
+var vm = new Vue({
+  el: '#vue-root',
+  delimiters: ["${", "}"],
+  data: {
+    message: 'Hello Vue!',
+  },
+  components: { Post, Blog },
 });
--- website/scripts/landing.js
+++ website/scripts/landing.js
@@ -1,18 +1,2 @@
-import Vue from 'vue';
-
-import Post from './Post.vue';
-import Blog from './Blog.vue';
 
 import css from 'Styles/landing.scss';
-
-/* Plugins come before new Vue instance */
-Vue.use(require('vue-moment'));
-
-var vm = new Vue({
-  el: '#vue-root',
-  delimiters: ["${", "}"],
-  data: {
-    message: 'Hello Vue!',
-  },
-  components: { Post, Blog },
-});
