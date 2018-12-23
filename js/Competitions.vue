af8bd1521f7271b8c1d9c8dbc6bc6543a6200adc
Carlos Requena López <crequena@ulb.ac.be>
2018-12-23 18:51:13 +0100

    add banner in tournaments section

--- js/Competitions.vue
+++ js/Competitions.vue
@@ -1,5 +1,6 @@
 <template>
    <v-container class="mt-3" v-bind="{ [`grid-list-xl`]: true }">
+      <v-img aspect-ratio="5" src="media/banner_tournaments.jpg"></v-img>
       <v-layout row wrap>
          <v-flex xs12>
             <h2 class="display-1">Tournaments and competitions</h2>
--- webpack.dev.js
+++ webpack.dev.js
@@ -29,7 +29,7 @@ module.exports = merge(common, {
   },
   devServer: {
     proxy: {
-      '/media/*.png': {
+      '/media/*.(png|jpg|jpeg)': {
         target: "http://localhost:8000",
         secure: false
       }
